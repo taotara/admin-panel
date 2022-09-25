e0a441344881018ba235769e919d788a9432eaba
taotara <taotara@yahoo.com>
2022-09-25 18:07:16 +0100

    checking for update

--- src/components/chart/chart.jsx
+++ src/components/chart/chart.jsx
@@ -13,11 +13,11 @@ const data = [
   { name: "June", Total: 1700 },
 ];
 
-function chart() {
+function chart({aspect}) {
   return (
     <div className='chart'>
         <div className="title">Last 6 Months (Revenue)</div>
-        <ResponsiveContainer width="100%" aspect={2 /1}>
+        <ResponsiveContainer width="100%" aspect = {aspect}>
         <AreaChart width={730} height={250} data={data}
   margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
   <defs>
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import './single.css'
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Chart from '../../components/chart/chart';
 
 function Single() {
   return (
-    <div>Single Page</div>
+    <div className='single'>
+      <Sidebar/>
+      <div className="singleContainer">
+        <Navbar/>
+        <div className="top">
+          <div className="left">
+            <h1 className="slTitle">Information</h1>
+            <div className="editButton">Edit</div>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/13636706/pexels-photo-13636706.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" className="itemImg" />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailitem">
+                  <span className="itemKey">Email: </span>
+                  <span className="itemValue">janedoe@mail.com</span>
+                </div>
+                <div className="detailitem">
+                  <span className="itemKey">Phone: </span>
+                  <span className="itemValue">+555 555 5555</span>
+                </div>
+                <div className="detailitem">
+                  <span className="itemKey">Address: </span>
+                  <span className="itemValue">Elton St. 234 Garden Yd. Newyork</span>
+                </div>
+                <div className="detailitem">
+                  <span className="itemKey">Country: </span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3/1} />
+          </div>
+        </div>
+        <div className="buttom"></div>
+      </div>
+    </div>
   )
 }
 
