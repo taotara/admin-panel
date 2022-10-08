20e4c9fc161309b432bf76d709f886d716e8821e
taotara <taotara@yahoo.com>
2022-10-08 17:15:11 +0100

    intergration of firebase

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate,
 } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import './style/dark.css';
@@ -17,24 +18,30 @@ import { DarkModeContext } from './context/darkModeContext';
 
 
 function App() {
+  const {darkMode} = useContext(DarkModeContext);
 
-  const {darkMode} = useContext(DarkModeContext)
+  const currentUser = false
+
+  const RequireAuth = ({children}) => {
+    return currentUser ? (children) : <Navigate to="/login"/>
+  }
+
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home/>}/>
             <Route path="login" element={<Login/>}/>
+            <Route index element={<RequireAuth><Home/></RequireAuth>}/>
               <Route path="users">
-                <Route index element={<List/>}/>
-                <Route path=":userId" element={<Single/>}/>
-                <Route path="new" element={<New inputs={userInputs} title="Add New User"/>}/>
+                <Route index element={<RequireAuth><List/></RequireAuth>}/>
+                <Route path=":userId" element={<RequireAuth><Single/></RequireAuth>}/>
+                <Route path="new" element={<RequireAuth><New inputs={userInputs} title="Add New User"/></RequireAuth>}/>
               </Route>
               <Route path="products">
-                <Route index element={<List/>}/>
-                <Route path=":productId" element={<Single/>}/>
-                <Route path="new" element={<New inputs={productInputs} title="Add New Product"/>}/>
+                <Route index element={<RequireAuth><List/></RequireAuth>}/>
+                <Route path=":productId" element={<RequireAuth><Single/></RequireAuth>}/>
+                <Route path="new" element={<RequireAuth><New inputs={productInputs} title="Add New Product"/></RequireAuth>}/>
               </Route>
           </Route>
         </Routes>
